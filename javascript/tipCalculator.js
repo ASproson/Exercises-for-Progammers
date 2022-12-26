35a54ad5d34aa4a9c5befdcd9de9250a75422e4b
atlas <a.sproson@outlook.com>
2022-12-26 11:55:46 +0000

    Adds basic input

--- javascript/tipCalculator.js
+++ javascript/tipCalculator.js
@@ -1,9 +1,27 @@
 "use strict";
 // Create a simple tip calculator. The program should prompt
 // for a bill amount and a tip rate. The program must compute
-// the tip and then display both the tip and the total amount of
-// the bill
+// the tip and then display both the tip and the total amount of the bill
+// Round fractions up to two decimal places
+// bill: 10
+// tip: 15
+// tip === 1.50, total === 11.50
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+let promptBill;
+let promptTip;
 const tipCalculator = (bill, tip) => {
     return "ri";
 };
+rl.question("What is the bill? ", function (bill) {
+    promptBill = bill;
+    rl.question("What is the tip percentage as a whole number? ", function (tip) {
+        promptTip = tip;
+        console.log(tipCalculator(promptBill, promptTip));
+        rl.close();
+    });
+});
 module.exports = tipCalculator;
--- javascript/tipCalculator.ts
+++ javascript/tipCalculator.ts
@@ -1,10 +1,34 @@
 // Create a simple tip calculator. The program should prompt
 // for a bill amount and a tip rate. The program must compute
-// the tip and then display both the tip and the total amount of
-// the bill
+// the tip and then display both the tip and the total amount of the bill
+// Round fractions up to two decimal places
+// bill: 10
+// tip: 15
+// tip === 1.50, total === 11.50
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let promptBill: number;
+let promptTip: number;
 
 const tipCalculator = (bill: number, tip: number): string => {
   return "ri";
 };
+
+rl.question("What is the bill? ", function (bill: number) {
+  promptBill = bill;
+  rl.question(
+    "What is the tip percentage as a whole number? ",
+    function (tip: number) {
+      promptTip = tip;
+      console.log(tipCalculator(promptBill, promptTip));
+      rl.close();
+    }
+  );
+});
 
 module.exports = tipCalculator;
