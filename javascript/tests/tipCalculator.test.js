685cdd0ad38a353d1b1bd4e87c177e2ffead917b
atlas <a.sproson@outlook.com>
2022-12-26 13:28:06 +0000

    Completes tipCalculator with TDD

--- javascript/tests/tipCalculator.test.js
+++ javascript/tests/tipCalculator.test.js
@@ -1,7 +1,31 @@
 const tipCalculator = require("../tipCalculator");
 
 describe("tipCalculator()", () => {
+  const answerOne = "Bill: 100, Tip: 10%, Total: 110";
+  const answerTwo = "Bill: 100, Tip: 15%, Total: 115";
+  const answerThree = "Bill: 57, Tip: 15%, Total: 65.55";
+  const answerFour = "Bill: 233, Tip: 17%, Total: 272.61";
+  const answerFive = "Please enter a number";
+
   it("returns a string", () => {
     expect(typeof tipCalculator(2, 3)).toEqual("string");
-  });
+  }),
+    it("returns answerOne when passed 100, 10", () => {
+      expect(tipCalculator(100, 10)).toEqual(answerOne);
+    }),
+    it("returns answerTwo when passed 100, 15", () => {
+      expect(tipCalculator(100, 15)).toEqual(answerTwo);
+    }),
+    it("returns answerThree when passed 57, 15", () => {
+      expect(tipCalculator(57, 15)).toEqual(answerThree);
+    }),
+    it("returns answerFour when passed 233, 17", () => {
+      expect(tipCalculator(233, 17)).toEqual(answerFour);
+    }),
+    it("returns error when passed aaaa, wwww", () => {
+      expect(tipCalculator("aaaa", "wwww")).toEqual(answerFive);
+    }),
+    it("returns error when passed negative num", () => {
+      expect(tipCalculator(-2, -10)).toEqual(answerFive);
+    });
 });
--- javascript/tipCalculator.ts
+++ javascript/tipCalculator.ts
@@ -6,29 +6,37 @@
 // tip: 15
 // tip === 1.50, total === 11.50
 
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-let promptBill: number;
-let promptTip: number;
+// let promptBill: number;
+// let promptTip: number;
 
 const tipCalculator = (bill: number, tip: number): string => {
-  return "ri";
+  if (
+    typeof bill !== "number" ||
+    typeof tip !== "number" ||
+    bill < 0 ||
+    tip < 0
+  ) {
+    return "Please enter a number";
+  }
+  return `Bill: ${bill}, Tip: ${tip}%, Total: ${bill + bill * (tip / 100)}`;
 };
 
-rl.question("What is the bill? ", function (bill: number) {
-  promptBill = bill;
-  rl.question(
-    "What is the tip percentage as a whole number? ",
-    function (tip: number) {
-      promptTip = tip;
-      console.log(tipCalculator(promptBill, promptTip));
-      rl.close();
-    }
-  );
-});
-
 module.exports = tipCalculator;
+
+// rl.question("What is the bill? ", function (bill: number) {
+//   promptBill = bill;
+//   rl.question(
+//     "What is the tip percentage as a whole number? ",
+//     function (tip: number) {
+//       promptTip = tip;
+//       console.log(tipCalculator(promptBill, promptTip));
+//       rl.close();
+//     }
+//   );
+// });
