cf82fdc7c56b929303de5f903640700e4a5ddd2c
atlas <a.sproson@outlook.com>
2022-12-28 13:27:01 +0000

    Completes TipCalculator

--- javascript/React/tipcalculator/src/App.tsx
+++ javascript/React/tipcalculator/src/App.tsx
@@ -1,23 +1,52 @@
-import React from "react";
-import { TipSlider } from "./components/TipSlider";
-import { UserInput } from "./components/UserInput";
-// import { Total } from "./components/Total";
+import React, { useEffect, useState } from "react";
 
 import "./App.css";
 
 function App() {
-  const callData = (str: string): void => {
-    console.log(str);
+  const [rangeValue, setRangeValue] = useState(15);
+  const [bill, setBill] = useState<number>(0);
+  const [total, setTotal] = useState<number>(0);
+
+  const handleBillInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBill(Number(e.target.value));
   };
 
-  // https://blog.devgenius.io/how-to-pass-data-from-child-to-parent-in-react-33ed99a90f43
+  const tipCalculator = (bill: number, tip: number): number => {
+    return bill + bill * (tip / 100);
+  };
+
+  useEffect(() => {
+    setTotal(tipCalculator(bill, rangeValue));
+  }, [rangeValue, bill]);
 
   return (
-    <div className="text-center">
-      <h1 className="text-3xl pt-4">Tip Calculator</h1>
-      <UserInput callData={callData} />
-      <TipSlider />
-      {/* <Total /> */}
+    <div className="text-center pt-[100px]">
+      <h1 className="text-3xl pt-4 font-bold">Tip Calculator</h1>
+      <div className="pt-8">
+        <h2 className="text-xl font-bold pb-2">Bill:</h2>
+        <input
+          type="number"
+          min="0"
+          value={bill ? bill : 0}
+          onChange={handleBillInput}
+          className="border-[1px] border-black rounded-md px-2"
+        />
+      </div>
+      <div className="pt-8">
+        <h2 className="font-bold text-xl pb-2">How was your service?</h2>
+        <input
+          type="range"
+          min={5}
+          max={20}
+          value={rangeValue}
+          onChange={(e) => setRangeValue(Number(e.target.value))}
+        />
+        <p className="text-lg font-bold">{rangeValue}</p>
+      </div>
+      <div className="pt-8">
+        <h2 className="font-bold text-xl pb-2">Total:</h2>
+        <p className="font-bold text-lg">{total.toFixed(2)}</p>
+      </div>
     </div>
   );
 }
